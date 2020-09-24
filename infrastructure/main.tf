42d384db3be381ef74d4e6637f64dd73cc8f0b77
chrisgrimble <chris.grimble@hmcts.net>
2020-09-24 13:45:44 +0100

    Load secrets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,6 +4,9 @@ provider "azurerm" {
 
 locals {
   S2S_SECRET_WORKFLOW_API = "${data.azurerm_key_vault_secret.s2s_secret.value}"
+  IA_IDAM_REDIRECT_URI = "${data.azurerm_key_vault_secret.idam_redirect_uri.value}"
+  TEST_LAW_FIRM_A_USERNAME = "${data.azurerm_key_vault_secret.idam_username.value}"
+  TEST_LAW_FIRM_A_PASSWORD = "${data.azurerm_key_vault_secret.idam_password.value}"
 }
 
 data "azurerm_key_vault" "wa_key_vault" {
@@ -16,7 +19,28 @@ data "azurerm_key_vault" "s2s_key_vault" {
   resource_group_name = "rpe-service-auth-provider-${var.env}"
 }
 
+data "azurerm_key_vault" "ia_key_vault" {
+  name                = "ia-${var.env}"
+  resource_group_name = "rpe-service-auth-provider-${var.env}"
+}
+
 data "azurerm_key_vault_secret" "s2s_secret" {
   key_vault_id = data.azurerm_key_vault.s2s_key_vault.id
   name         = "microservicekey-wa-workflow-api"
 }
+
+data "azurerm_key_vault_secret" "idam_redirect_uri" {
+  key_vault_id = data.azurerm_key_vault.ia_key_vault.id
+  name         = "idam-redirect-uri"
+}
+
+data "azurerm_key_vault_secret" "idam_username" {
+  key_vault_id = data.azurerm_key_vault.ia_key_vault.id
+  name         = "test-law-firm-a-username"
+}
+
+data "azurerm_key_vault_secret" "idam_password" {
+  key_vault_id = data.azurerm_key_vault.ia_key_vault.id
+  name         = "test-law-firm-a-password"
+}
+
