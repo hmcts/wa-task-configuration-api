0762b31e41b0ff446993971de8cda16f4d9e6d57
chrisgrimble <chris.grimble@hmcts.net>
2020-09-24 15:17:11 +0100

    Try and load vault values

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,6 +7,11 @@ locals {
   IA_IDAM_REDIRECT_URI = "${data.azurerm_key_vault_secret.idam_redirect_uri.value}"
   TEST_LAW_FIRM_A_USERNAME = "${data.azurerm_key_vault_secret.idam_username.value}"
   TEST_LAW_FIRM_A_PASSWORD = "${data.azurerm_key_vault_secret.idam_password.value}"
+
+    ia_preview_vault_name     = "ia-aat"
+    ia_non_preview_vault_name = "ia-${var.env}"
+    ia_key_vault_name         = "${var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name}"
+
 }
 
 data "azurerm_key_vault" "wa_key_vault" {
@@ -20,8 +25,8 @@ data "azurerm_key_vault" "s2s_key_vault" {
 }
 
 data "azurerm_key_vault" "ia_key_vault" {
-  name                = "ia-${var.env}"
-  resource_group_name = "rpe-service-auth-provider-${var.env}"
+  name                = "${ia_key_vault_name}"
+  resource_group_name = "${ia_key_vault_name}"
 }
 
 data "azurerm_key_vault_secret" "s2s_secret" {
