d57dfcb098c8846084cef81560e8dad90854e7c8
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-12-14 12:39:59 +0000

    disable timestamp feature

--- src/main/java/uk/gov/hmcts/reform/wataskconfigurationapi/config/JacksonConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/wataskconfigurationapi/config/JacksonConfiguration.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.wataskconfigurationapi.config;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
@@ -26,7 +27,8 @@ public class JacksonConfiguration {
                 new JavaTimeModule(),
                 new ParameterNamesModule(),
                 new Jdk8Module()
-            );
+            )
+            .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 
     @Bean
