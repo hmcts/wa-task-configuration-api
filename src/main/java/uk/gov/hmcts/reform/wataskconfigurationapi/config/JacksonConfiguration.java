8e34db73cb9befcf823ee101f025c1460fd4a882
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-12-15 09:49:14 +0000

    fix

--- src/main/java/uk/gov/hmcts/reform/wataskconfigurationapi/config/JacksonConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/wataskconfigurationapi/config/JacksonConfiguration.java
@@ -37,6 +37,7 @@ public class JacksonConfiguration {
         // Set default date to RFC3339 standards
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.ENGLISH);
         objectMapper.setDateFormat(df);
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
         return objectMapper;
     }
 
