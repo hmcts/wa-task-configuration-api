da06fc5c0a1f038719ef93e06db947881ee7afd3
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-08-10 14:52:05 +0100

    Add jacson configuration

--- src/main/java/uk/gov/hmcts/reform/wataskconfigurationapi/config/JacksonConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/wataskconfigurationapi/config/JacksonConfiguration.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.wataskconfigurationapi.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
@@ -11,9 +10,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 import java.text.SimpleDateFormat;
 import java.util.Locale;
+
+import static com.fasterxml.jackson.databind.DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE;
 
 @Configuration
 public class JacksonConfiguration {
@@ -21,9 +23,13 @@ public class JacksonConfiguration {
     @Bean
     @Primary
     public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+        // Set default date to RFC3339 standards
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.ENGLISH);
         return new Jackson2ObjectMapperBuilder()
-            .serializationInclusion(JsonInclude.Include.NON_ABSENT)
             .propertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+            .serializationInclusion(JsonInclude.Include.NON_ABSENT)
+            .featuresToEnable(READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE)
+            .dateFormat(df)
             .modules(
                 new ParameterNamesModule(),
                 new JavaTimeModule(),
@@ -33,14 +39,16 @@ public class JacksonConfiguration {
 
     @Bean
     @Primary
-    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
-        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
-        objectMapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true);
-        // Set default date to RFC3339 standards
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.ENGLISH);
-        objectMapper.setDateFormat(df);
-        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
-        return objectMapper;
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        Jackson2ObjectMapperBuilder builder = jackson2ObjectMapperBuilder();
+        return new MappingJackson2HttpMessageConverter(builder.build());
+    }
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        Jackson2ObjectMapperBuilder builder = jackson2ObjectMapperBuilder();
+        return builder.createXmlMapper(false).build();
     }
 
 }
